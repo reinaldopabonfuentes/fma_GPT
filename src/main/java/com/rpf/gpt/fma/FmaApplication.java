a65a734951a8ccbe1e1077d5975f023f3e90b6ff
reinaldopabonfuentes <reinaldopabonfuentes@gmail.com>
2023-09-25 19:56:16 -0500

    Kickoff commit

---
+++ src/main/java/com/rpf/gpt/fma/FmaApplication.java
@@ -0,0 +1,13 @@
+package com.rpf.gpt.fma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmaApplication.class, args);
+	}
+
+}
